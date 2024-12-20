9c288d63c55c2c9054eaa716b87c73c2bf922c70
Subha6292 <142993261+Subha6292@users.noreply.github.com>
2024-12-20 12:07:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('.rider-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you! A download link has been sent to your mobile.');
+});
